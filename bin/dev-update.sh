ea1f9986849449ab0a3ae0e444d50009160db57e
Jim Simon <jim.j.simon@gmail.com>
2020-10-21 23:57:45 -0400

    Add dev-update script

---
+++ bin/dev-update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+docker-compose build --pull
+docker-compose run --rm api npm install --ignore-scripts
+docker-compose run --rm api npm run bootstrap
+docker-compose run --rm api bash -c 'cd packages/api && npx prisma-multi-tenant generate'
+docker-compose run --rm api bash -c 'cd packages/api && npx prisma-multi-tenant migrate up'
